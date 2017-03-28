d994ad6db64d7a6d74fb7bbfca17e80c849b9017
alitaso345 <a.maruyama@uec.ac.jp>
2017-03-28 11:23:32 +0900

    Add SearchWorks struct

---
+++ AnnictAPIClient/SearchWorks.swift
@@ -0,0 +1,45 @@
+struct SearchWorks : JSONDecodable {
+    let total_count: Int
+    let next_page: Int?
+    let prev_page: Int?
+    let works: [Anime]
+
+    init(json: Any) throws {
+        guard let dictionary = json as? [String : Any] else {
+            throw JSONDecodeError.invalidFormat(json: json)
+        }
+
+        guard let total_count = dictionary["total_count"] as? Int else {
+            throw JSONDecodeError.missingValue(
+                key: "total_count",
+                actualValue: dictionary["total_count"])
+        }
+
+        guard let next_page = dictionary["next_page"] as? Int else {
+            throw JSONDecodeError.missingValue(
+                key: "next_page",
+                actualValue: dictionary["next_page"])
+        }
+
+        guard let prev_page = dictionary["prev_page"] as? Int else {
+            throw JSONDecodeError.missingValue(
+                key: "prev_page",
+                actualValue: dictionary["prev_page"])
+        }
+
+        guard let workObjects = dictionary["works"] as? [Any] else {
+            throw JSONDecodeError.missingValue(
+                key: "works",
+                actualValue: dictionary["works"])
+        }
+
+        let works = try workObjects.map {
+            return try Anime(json: $0)
+        }
+
+        self.total_count = total_count
+        self.next_page = next_page
+        self.prev_page = prev_page
+        self.works = works
+    }
+}
