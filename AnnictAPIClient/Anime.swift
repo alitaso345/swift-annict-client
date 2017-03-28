08e74081e4b3ac93d886043c74f6cf6b2bbdc744
alitaso345 <a.maruyama@uec.ac.jp>
2017-03-28 10:33:35 +0900

    Add Anime struct

---
+++ AnnictAPIClient/Anime.swift
@@ -0,0 +1,17 @@
+struct Anime {
+    let id: Int
+    let title: String
+    let title_kana: String
+    let media: String
+    let media_text: String
+    let released_on: String
+    let released_on_about: String
+    let official_site_url: String
+    let wikipedia_url: String
+    let twitter_username: String
+    let twitter_hashtag: String
+    let episodes_count: Int
+    let watchers_count: Int
+    let season_name: String
+    let season_name_text: String
+}
