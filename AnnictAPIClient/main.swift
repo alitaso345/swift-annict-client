df708bcdf444a4b54a5ca06d3342de1b5fd00425
alitaso345 <a.maruyama@uec.ac.jp>
2017-03-28 21:54:25 +0900

    Add main.swift

--- AnnictAPIClient/main.swift
+++ AnnictAPIClient/main.swift
@@ -1,12 +1,24 @@
-//
-//  main.swift
-//  AnnictAPIClient
-//
-//  Created by arisu-maruyama on 2017/03/28.
-//  Copyright © 2017年 alitaso345. All rights reserved.
-//
-
 import Foundation
 
-print("Hello, World!")
+print("Annict Client")
+print("Enter your annict access token> ", terminator: "")
 
+guard let annictAccessToken = readLine(strippingNewline: true) else {
+    exit(1)
+}
+
+let client = AnnictClient(accessToken: annictAccessToken)
+let request = AnnictAPI.SearchWorks()
+
+client.send(request: request) { result in
+    switch result {
+    case let .success(response):
+        for work in response.works {
+            print(work.title)
+        }
+        //exit(0)
+    case let .failure(error):
+        print(error)
+        exit(1)
+    }
+}
